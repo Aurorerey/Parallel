d485f509eb14ef18d1e48f09f4c83c4250b17b3d
Ludovic Jouenne <goldeneagles@protonmail.com>
2021-03-15 21:33:59 +0100

    Creation des routes sans les routes statiques

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :users, only: [:edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :cabinets do
+    resources :reservations, only: [:new, :create, :destroy]
+  end
 end
