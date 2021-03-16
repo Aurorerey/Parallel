894254a805426f0011c4a2c4452b1b35b46d5d77
Ludovic Jouenne <goldeneagles@protonmail.com>
2021-03-16 09:56:45 +0100

    finalisation des routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  # Edit profil of user
   resources :users, only: [:edit, :update]
+  # Index and show for activities
+  resources :categories, only: [] do
+    resources :activities, only: [:index, :show]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :cabinets do
     resources :reservations, only: [:new, :create, :destroy]
