dae00ba0d9f48da9e1dbe749e958ea788849c7dd
Ludovic Jouenne <goldeneagles@protonmail.com>
2021-03-23 16:04:51 +0100

    supercalendar/map

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,10 @@ class PagesController < ApplicationController
     @cabinets = Cabinet.all
     @reservations = current_user.reservations
   end
+
+  def destroy
+    @reservation = Reservation.find(params[:id])
+    @reservation.destroy
+    redirect_to cabinet_path, notice: 'Cabinet was successfully destroyed.'
+  end
 end
